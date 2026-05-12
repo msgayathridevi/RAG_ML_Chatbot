35fa1e7c487a51e36a88562c6d41dcdeaae41484
Gayathri Devi MS <msg3dv@gmail.com>
2026-05-12 10:43:00 +0530

    backend app

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request
-from src.helper import get_embedding
-from langchain_openai import ChatOpenAI
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
-from src.prompt import *
 import os
+
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+from src.helper import get_embedding
+from src.prompt import *
+from src.prompts import *
 from store import docs
 
 app = Flask(__name__)
@@ -14,16 +18,18 @@ app = Flask(__name__)
 load_dotenv()
 
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 embeddings = get_embedding()
 
 
 retriever = docs.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-llm = ChatOpenAI(model = "gpt-4o-mini", temperature=0.4)
-
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",  
+    temperature=0.7
+)
 
 # Create prompt templates
 base_prompt = ChatPromptTemplate.from_messages([
